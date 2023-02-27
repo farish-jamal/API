eb249f31a0301328d7df6524f7f52f3683bb3be6
Farish Jamal <farishjamal98@gmail.com>
2023-02-27 17:07:37 +0530

    Last

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) =>{
     if(req.url == "/"){
+        res.end("Hello From Home Page!");
+    }else if(req.url == "/data"){
         fs.readFile(`${__dirname}/data.json`, "utf-8", (err, data) =>{
             const objData = data;
             res.end(objData);
